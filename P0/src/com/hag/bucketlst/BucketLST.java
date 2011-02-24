969b015ee54893a817fe3202d8f3c69edc0a357d
Tamara <tfleish@mit.edu>
2011-02-23 20:49:15 -0500

    another test push, from terminal

---
+++ P0/src/com/hag/bucketlst/BucketLST.java
@@ -0,0 +1,13 @@
+package com.hag.bucketlst;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BucketLST extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
