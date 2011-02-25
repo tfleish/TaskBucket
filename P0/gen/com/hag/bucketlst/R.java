ab1c5b7c8c83f580f12a0f55403f06f8c212fb73
sliverb <lilibayo@mit.edu>
2011-02-25 14:55:48 -0500

    Fix

--- P0/gen/com/hag/bucketlst/R.java
+++ P0/gen/com/hag/bucketlst/R.java
@@ -15,7 +15,7 @@ public final class R {
     }
     public static final class id {
         public static final int add=0x7f050002;
-        public static final int spinner1=0x7f050001;
+        public static final int categoryGet=0x7f050001;
         public static final int titleGet=0x7f050000;
     }
     public static final class layout {
